57c9ee9161295f1143ca8f6940207ab79d80136a
hj5230 <hf305@foxmail.com>
2024-04-05 23:32:14 +0300

    basic functionality done

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 	"time"
@@ -10,43 +12,86 @@ import (
 	"github.com/chromedp/cdproto/network"
 	"github.com/chromedp/chromedp"
 	"github.com/gin-gonic/gin"
+	ffmpeg_go "github.com/u2takey/ffmpeg-go"
 )
 
 func biliAudioGetter(c *gin.Context) {
-	BV := c.Param("BV")
+	BV := c.Query("BV")
+	oFormat := c.DefaultQuery("format", "mp3")
+	bitrate := c.DefaultQuery("bitrate", "128k")
 	// check BV, if malformed raise exception
 	url := fmt.Sprintf("https://www.bilibili.com/video/%s", BV)
 
+	var matches []string
+
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
-
 	chromedp.ListenTarget(ctx, func(ev interface{}) {
 		switch ev := ev.(type) {
 		case *network.EventRequestWillBeSent:
 			if ev.Type == "Fetch" {
 				url := ev.Request.URL
 				if strings.Contains(url, "upos-hz-mirrorakam.akamaized.net") {
-					fmt.Println(url)
+					matches = append(matches, url)
 				}
 			}
 		}
 	})
-
 	err := chromedp.Run(ctx,
 		chromedp.Navigate(url),
 		chromedp.Sleep(5*time.Second),
 	)
 	if err != nil {
-		c.JSON(http.StatusNotAcceptable, gin.H{"msg": "Unable to load URL"})
+		c.JSON(http.StatusNotAcceptable, gin.H{"msg": "Unable to load load web page"})
+		return
+	} else if len(matches) < 2 {
+		c.JSON(http.StatusNotFound, gin.H{"msg": "Audio URL not found"})
 		return
 	}
-	// ...
+
+	fmt.Println(matches[1])
+
+	res, err := http.Get(matches[1])
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": "Failed to fetch audio"})
+		return
+	}
+	defer res.Body.Close()
+
+	audio, err := io.ReadAll(res.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": "Failed to read audio data"})
+		return
+	}
+	iBuffer := bytes.NewBuffer(audio)
+	oBuffer := bytes.NewBuffer(nil)
+
+	err = ffmpeg_go.
+		Input("pipe:", ffmpeg_go.KwArgs{"f": "mp4"}).
+		Output("pipe:", ffmpeg_go.KwArgs{"f": oFormat, "acodec": "libmp3lame", "b:a": bitrate}).
+		WithInput(iBuffer).
+		WithOutput(oBuffer).
+		Run()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": "Failed to convert audio"})
+		return
+	}
+
+	c.Header("Content-Type", "audio/mpeg")
+	c.Header("Content-Disposition", fmt.Sprintf("attachment; filename=audio.%s", oFormat))
+
+	_, err = c.Writer.Write(oBuffer.Bytes())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": "Failed to write response"})
+		return
+	}
 }
 
 func main() {
 	r := gin.Default()
+	r.SetTrustedProxies(nil)
 
-	r.GET("/:BV", biliAudioGetter)
+	r.GET("/", biliAudioGetter)
 
 	r.Run(":8081")
 }
